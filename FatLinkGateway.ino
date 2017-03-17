4df3637b47806d0691841369c6f3b62199716e1e
Mikael Falkvidd git@mjo.se <git@mjo.se>
2017-03-17 19:42:11 +0100

    Initial commit

---
+++ FatLinkGateway.ino
@@ -0,0 +1,13 @@
+/*
+   Logs received messages to syslog with snr and rssi
+   Logs received messages to pubnub
+   Sends ack on received messages with snr and rssi
+   Sends beacon with regular intervals
+*/
+void setup() {
+
+}
+
+void loop() {
+
+}
